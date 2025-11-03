9ade2b4fd259412d2c6b5d6ea1cfc6b6ca875b65
flacko9899 <abbey9899@gmail.com>
2025-11-03 14:31:17 -0500

    Your message

--- input.js
+++ input.js
@@ -1,2 +1,33 @@
-//show or hide password 
-let passcode = document.getElementById('password');
+// Client side validation
+const sly = document.getElementById('loginForm');
+sly.addEventlistener('submit',(e) => {
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const error = document.getElementById('error');
+const passerror = document.getElementById('passError');
+let isValid = true;
+
+//Email Validation expression
+const empattern =    /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if(empattern.test(email)) {
+    error.style.display = 'block';
+    isValid = false;
+}
+else {
+    error.style.display = 'none';
+}
+
+// Password Validation expression
+if(password<13) {
+    passerror.style.display = 'block';
+    isValid = false;
+}
+else {
+    passerror.style.display = 'none';
+}
+if(!isValid) {
+    e.preventDefault();
+}
+
+
+})
